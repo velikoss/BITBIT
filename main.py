c641bc2d6079f9b8db481225c8d567a3ab3c0c1e
FvipF <61709999+FvipF@users.noreply.github.com>
2021-12-19 12:50:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import re
 import requests
 
-a = input().split()
+a = input().split()  # от бота мы получаем название
+count = 0
+tovar = 'konsol'  # от бота мы получаем консоль это или видеокарта
 s = "+".join(a)
 url = f'https://www.citilink.ru/search/?text='
 url += s
@@ -9,9 +11,17 @@ response = requests.get(url)
 b = re.findall(r'(https?://\S+)', response.text)
 all_urls = []
 for i in b:
-    if 'product' in i and 'add' not in i:
-        all_urls.append(i.split('"')[0])
+    if tovar == 'konsol':
+        if 'product' in i and 'add' not in i and 'konsol' in i:
+            all_urls.append(i.split('"')[0])
+    elif tovar == 'pk':
+        if 'product' in i and 'add' not in i and 'videokarta' in i:
+            all_urls.append(i.split('"')[0])
 for i in all_urls:
+    if count >= 3:
+        break
     abc = requests.get(i)
     if 'InStock' in abc.text:
-        print(i)
+        print(i)  # вывод ботом
+if count == 0:
+    print("Сейчас такого товара в наличии нет")  # вывод ботом

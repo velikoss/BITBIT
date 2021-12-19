202960f2f9e73d3ae91a0b30bf4094fcd63833ec
FvipF <61709999+FvipF@users.noreply.github.com>
2021-12-19 11:33:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import re
 import requests
-from bs4 import BeautifulSoup
 
 a = input().split()
 s = "+".join(a)
 url = f'https://www.citilink.ru/search/?text='
 url += s
 response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
 b = re.findall(r'(https?://\S+)', response.text)
 all_urls = []
 for i in b:
@@ -15,6 +13,5 @@ for i in b:
         all_urls.append(i.split('"')[0])
 for i in all_urls:
     abc = requests.get(i)
-    soup = BeautifulSoup(abc.text, 'lxml')
     if 'InStock' in abc.text:
         print(i)
